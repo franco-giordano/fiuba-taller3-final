b759d8fddbaee201b81ece04f864723e73193611
Julian Ferres <julianferres@gmail.com>
2021-12-08 00:41:03 -0300

    Elimino codigo de la template view no usado

--- fiubamate-plugin/src/main/java/plugin/trackmate/examples/view/FIUBAmateView.java
+++ fiubamate-plugin/src/main/java/plugin/trackmate/examples/view/FIUBAmateView.java
@@ -61,12 +61,7 @@ public class FIUBAmateView extends JFrame
 
 	private final Model model;
 
-	// private final TablePanel< Spot > spotTable;
 	private Roi roi = null;
-
-	// private final AtomicBoolean ignoreSelectionChange = new AtomicBoolean(false);
-
-	// private final SelectionModel selectionModel;
 
 	private final JButton btnAgregarArea;
 	private final JButton btnExportarCSV;
@@ -81,7 +76,6 @@ public class FIUBAmateView extends JFrame
 		IJ.log("Cantidad de Tracks distintos en el modelo: " + model.getTrackModel().trackIDs(false).size());
 		setIconImage(TRACKMATE_ICON.getImage());
 		this.model = model;
-		// this.selectionModel = selectionModel;
 
 		/*
 		 * GUI.
@@ -130,21 +124,6 @@ public class FIUBAmateView extends JFrame
 		gbcBtnEditSettings.gridy = 0;
 		add(btnEditSettings, gbcBtnEditSettings);
 
-		/*
-		 * Display spot checkbox.
-		 */
-
-		// final JCheckBox chkboxDisplaySpots = new JCheckBox();
-		// chkboxDisplaySpots.setText("Display spots");
-		// chkboxDisplaySpots.setFont(FONT);
-		// final GridBagConstraints gbcCheckBoxDisplaySpots = new GridBagConstraints();
-		// gbcCheckBoxDisplaySpots.anchor = GridBagConstraints.NORTH;
-		// gbcCheckBoxDisplaySpots.fill = GridBagConstraints.HORIZONTAL;
-		// gbcCheckBoxDisplaySpots.insets = new Insets(0, 5, 0, 5);
-		// gbcCheckBoxDisplaySpots.gridx = 0;
-		// gbcCheckBoxDisplaySpots.gridy = 1;
-		// add(chkboxDisplaySpots, gbcCheckBoxDisplaySpots);
-
 		final JLabel lblContarCuerpos = new JLabel("Contabilizar cuerpos en Area");
 		lblContarCuerpos.setFont(FONT);
 		final GridBagConstraints gbcLblContarCuerpos = new GridBagConstraints();
@@ -154,21 +133,6 @@ public class FIUBAmateView extends JFrame
 		gbcLblContarCuerpos.gridx = 0;
 		gbcLblContarCuerpos.gridy = 1;
 		add(lblContarCuerpos, gbcLblContarCuerpos);
-
-		// final JCheckBox chkboxDisplaySpotsAsRois = new JCheckBox();
-		// chkboxDisplaySpotsAsRois.setText("as ROIs");
-		// final GridBagConstraints gbcChkboxDisplaySpotsAsRois = new
-		// GridBagConstraints();
-		// gbcChkboxDisplaySpotsAsRois.insets = new Insets(0, 0, 0, 5);
-		// gbcChkboxDisplaySpotsAsRois.anchor = GridBagConstraints.EAST;
-		// gbcChkboxDisplaySpotsAsRois.gridx = 1;
-		// gbcChkboxDisplaySpotsAsRois.gridy = 1;
-		// add(chkboxDisplaySpotsAsRois, gbcChkboxDisplaySpotsAsRois);
-		// chkboxDisplaySpotsAsRois.setFont(FONT);
-		// chkboxDisplaySpotsAsRois
-		// .addActionListener(e ->
-		// ds.setSpotDisplayedAsRoi(chkboxDisplaySpotsAsRois.isSelected()));
-		// chkboxDisplaySpotsAsRois.setSelected(ds.isSpotDisplayedAsRoi());
 
 		/*
 		 * Spot options panel.
@@ -187,15 +151,6 @@ public class FIUBAmateView extends JFrame
 		gblPanelSpotOptions.columnWeights = new double[] { 0.0, 1.0 };
 		gblPanelSpotOptions.rowWeights = new double[] { 0.0, 0.0 };
 		panelSpotOptions.setLayout(gblPanelSpotOptions);
-
-		// final JLabel lblSpotRadius = new JLabel("Spot display radius ratio:");
-		// lblSpotRadius.setFont(SMALL_FONT);
-		// final GridBagConstraints gbcLblSpotRadius = new GridBagConstraints();
-		// gbcLblSpotRadius.anchor = GridBagConstraints.EAST;
-		// gbcLblSpotRadius.insets = new Insets(5, 5, 0, 5);
-		// gbcLblSpotRadius.gridx = 0;
-		// gbcLblSpotRadius.gridy = 0;
-		// panelSpotOptions.add(lblSpotRadius, gbcLblSpotRadius);
 
 		btnAgregarArea = new JButton("Agregar Area", ADD_ICON);
 		btnAgregarArea.addActionListener(e -> onAgregarArea());
@@ -219,19 +174,6 @@ public class FIUBAmateView extends JFrame
 		gbcbtnExportarCSV.gridy = 1;
 		panelSpotOptions.add(btnExportarCSV, gbcbtnExportarCSV);
 
-		// final JFormattedTextField ftfSpotRadius = new JFormattedTextField();
-		// GuiUtils.selectAllOnFocus(ftfSpotRadius);
-		// ftfSpotRadius.setHorizontalAlignment(SwingConstants.CENTER);
-		// ftfSpotRadius.setFont(SMALL_FONT);
-		// ftfSpotRadius.setMinimumSize(new Dimension(80, 20));
-		// ftfSpotRadius.setColumns(5);
-		// final GridBagConstraints gbcFtfSpotRadius = new GridBagConstraints();
-		// gbcFtfSpotRadius.insets = new Insets(5, 0, 0, 0);
-		// gbcFtfSpotRadius.anchor = GridBagConstraints.WEST;
-		// gbcFtfSpotRadius.gridx = 1;
-		// gbcFtfSpotRadius.gridy = 0;
-		// panelSpotOptions.add(ftfSpotRadius, gbcFtfSpotRadius);
-
 		lblAmountAreasAdded = new JLabel("Cantidad de areas agregadas: " + addedAreas.size());
 		lblAmountAreasAdded.setFont(SMALL_FONT);
 		final GridBagConstraints gbclblAmountAreasAdded = new GridBagConstraints();
@@ -241,34 +183,6 @@ public class FIUBAmateView extends JFrame
 		gbclblAmountAreasAdded.gridy = 2;
 		panelSpotOptions.add(lblAmountAreasAdded, gbclblAmountAreasAdded);
 
-		// final JCheckBox chkboxSpotNames = new JCheckBox();
-		// final GridBagConstraints gbcChkboxSpotNames = new GridBagConstraints();
-		// gbcChkboxSpotNames.anchor = GridBagConstraints.WEST;
-		// gbcChkboxSpotNames.gridx = 1;
-		// gbcChkboxSpotNames.gridy = 1;
-		// panelSpotOptions.add(chkboxSpotNames, gbcChkboxSpotNames);
-
-		// final JPanel selectorForSpots = featureSelector.createSelectorForSpots();
-		// final GridBagConstraints gbcCmbboxSpotColor = new GridBagConstraints();
-		// gbcCmbboxSpotColor.insets = new Insets( 0, 5, 5, 5 );
-		// gbcCmbboxSpotColor.fill = GridBagConstraints.BOTH;
-		// gbcCmbboxSpotColor.gridwidth = 2;
-		// gbcCmbboxSpotColor.gridx = 0;
-		// gbcCmbboxSpotColor.gridy = 3;
-		// panelSpotOptions.add( selectorForSpots, gbcCmbboxSpotColor );
-
-		// final UpdateListener refresher = () -> refresh();
-		// ds.listeners().add(refresher);
-		// selectionModel.addSelectionChangeListener(this);
-		// model.addModelChangeListener(this);
-		// addWindowListener(new WindowAdapter() {
-		// 	@Override
-		// 	public void windowClosing(final java.awt.event.WindowEvent e) {
-		// 		// selectionModel.removeSelectionChangeListener(FIUBAmateView.this);
-		// 		// model.removeModelChangeListener(FIUBAmateView.this);
-		// 		ds.listeners().remove(refresher);
-		// 	};
-		// });
 		Roi.addRoiListener(this);
 	}
 
@@ -288,16 +202,6 @@ public class FIUBAmateView extends JFrame
 	}
 
 	public void exportToCsv(final String csvFile, String stats) {
-		// try
-		// {
-		// spotTable.exportToCsv( new File( csvFile ) );
-		// }
-		// catch ( final IOException e )
-		// {
-		// model.getLogger().error( "Problem exporting to file "
-		// + csvFile + "\n" + e.getMessage() );
-		// }
-
 		try (CSVWriter writer = new CSVWriter(new FileWriter(csvFile),
 				CSVWriter.DEFAULT_SEPARATOR,
 				CSVWriter.NO_QUOTE_CHARACTER,
@@ -312,90 +216,6 @@ public class FIUBAmateView extends JFrame
 		}
 	}
 
-	// public static final TablePanel< Spot > createSpotTable( final Model model,
-	// final DisplaySettings ds )
-	// {
-	// final List< String > features = new ArrayList<>(
-	// model.getFeatureModel().getSpotFeatures() );
-	// final Map< String, String > featureNames =
-	// model.getFeatureModel().getSpotFeatureNames();
-	// final Map< String, String > featureShortNames =
-	// model.getFeatureModel().getSpotFeatureShortNames();
-	// final Map< String, String > featureUnits = new HashMap<>();
-	// for ( final String feature : features )
-	// {
-	// final Dimension dimension =
-	// model.getFeatureModel().getSpotFeatureDimensions().get( feature );
-	// final String units = TMUtils.getUnitsFor( dimension, model.getSpaceUnits(),
-	// model.getTimeUnits() );
-	// featureUnits.put( feature, units );
-	// }
-	// final Map< String, Boolean > isInts =
-	// model.getFeatureModel().getSpotFeatureIsInt();
-	// final Map< String, String > infoTexts = new HashMap<>();
-	// final Function< Spot, String > labelGenerator = spot -> spot.getName();
-	// final BiConsumer< Spot, String > labelSetter = ( spot, label ) ->
-	// spot.setName( label );
-
-	// /*
-	// * Feature provider. We add a fake one to show the spot ID.
-	// */
-	// final String SPOT_ID = "ID";
-	// features.add( 0, SPOT_ID );
-	// featureNames.put( SPOT_ID, "Spot ID" );
-	// featureShortNames.put( SPOT_ID, "Spot ID" );
-	// featureUnits.put( SPOT_ID, "" );
-	// isInts.put( SPOT_ID, Boolean.TRUE );
-	// infoTexts.put( SPOT_ID, "The id of the spot." );
-
-	// /*
-	// * Feature provider. We add a fake one to show the spot *track* ID.
-	// */
-	// final String TRACK_ID = "TRACK_ID";
-	// features.add( 1, TRACK_ID );
-	// featureNames.put( TRACK_ID, "Track ID" );
-	// featureShortNames.put( TRACK_ID, "Track ID" );
-	// featureUnits.put( TRACK_ID, "" );
-	// isInts.put( TRACK_ID, Boolean.TRUE );
-	// infoTexts.put( TRACK_ID, "The id of the track this spot belongs to." );
-
-	// final BiFunction< Spot, String, Double > featureFun = ( spot, feature ) -> {
-	// if ( feature.equals( TRACK_ID ) )
-	// {
-	// final Integer trackID = model.getTrackModel().trackIDOf( spot );
-	// return trackID == null ? null : trackID.doubleValue();
-	// }
-	// else if ( feature.equals( SPOT_ID ) )
-	// return ( double ) spot.ID();
-
-	// return spot.getFeature( feature );
-	// };
-
-	// final Supplier< FeatureColorGenerator< Spot > > coloring =
-	// () -> FeatureUtils.createSpotColorGenerator( model, ds );
-
-	// final BiConsumer< Spot, Color > colorSetter =
-	// ( spot, color ) -> spot.putFeature( ManualSpotColorAnalyzerFactory.FEATURE,
-	// Double.valueOf( color.getRGB() ) );
-
-	// final TablePanel< Spot > table =
-	// new TablePanel<>(
-	// model.getSpots().iterable( true ),
-	// features,
-	// featureFun,
-	// featureNames,
-	// featureShortNames,
-	// featureUnits,
-	// isInts,
-	// infoTexts,
-	// coloring,
-	// labelGenerator,
-	// labelSetter,
-	// ManualSpotColorAnalyzerFactory.FEATURE,
-	// colorSetter );
-	// return table;
-	// }
-
 	@Override
 	public void render() {
 		setLocationRelativeTo(null);
@@ -406,54 +226,6 @@ public class FIUBAmateView extends JFrame
 	public void refresh() {
 		repaint();
 	}
-
-	// @Override
-	// public void modelChanged(final ModelChangeEvent event) {
-	// 	if (event.getEventID() == ModelChangeEvent.FEATURES_COMPUTED) {
-	// 		refresh();
-	// 		return;
-	// 	}
-
-	// 	final List<Spot> spots = new ArrayList<>();
-	// 	for (final Spot spot : model.getSpots().iterable(true))
-	// 		spots.add(spot);
-	// 	// spotTable.setObjects( spots );
-
-	// 	refresh();
-	// }
-
-	/*
-	 * Forward selection model changes to the tables.
-	 */
-	// @Override
-	// public void selectionChanged(final SelectionChangeEvent event) {
-	// 	if (ignoreSelectionChange.get())
-	// 		return;
-	// 	ignoreSelectionChange.set(true);
-
-	// 	// Vertices table.
-	// 	final Set<Spot> selectedVertices = selectionModel.getSpotSelection();
-	// 	// final JTable vt = spotTable.getTable();
-	// 	// vt.getSelectionModel().clearSelection();
-	// 	// for ( final Spot spot : selectedVertices )
-	// 	// {
-	// 	// final int row = spotTable.getViewRowForObject( spot );
-	// 	// vt.getSelectionModel().addSelectionInterval( row, row );
-	// 	// }
-
-	// 	// Center on selection if we added one spot exactly
-	// 	final Map<Spot, Boolean> spotsAdded = event.getSpots();
-	// 	if (spotsAdded != null && spotsAdded.size() == 1) {
-	// 		final boolean added = spotsAdded.values().iterator().next();
-	// 		if (added) {
-	// 			final Spot spot = spotsAdded.keySet().iterator().next();
-	// 			centerViewOn(spot);
-	// 		}
-	// 	}
-
-	// 	refresh();
-	// 	ignoreSelectionChange.set(false);
-	// }
 
 	@Override
 	public void centerViewOn(final Spot spot) {
@@ -495,14 +267,6 @@ public class FIUBAmateView extends JFrame
 			return;
 		}
 
-		// ambos printean null:
-		// IJ.log("DEBUG INFO: " + roi.getDebugInfo() + "\n");
-		// IJ.log("PROPS: " + roi.getProperties() + "\n");
-
-		// for (Point p : roi) {
-		// // process p
-		// IJ.log(p.toString());
-		// }
 		IJ.log(roi.getBounds().toString());
 
 		addedAreas.add(roi);
@@ -535,32 +299,4 @@ public class FIUBAmateView extends JFrame
 		IJ.log("Exportando CSV");
 		exportToCsv(String.valueOf(nFrames));
 	}
-
-	/**
-	 * Forward spot table selection to selection model.
-	 */
-	// private final class SpotTableSelectionListener implements
-	// ListSelectionListener
-	// {
-
-	// @Override
-	// public void valueChanged( final ListSelectionEvent event )
-	// {
-	// if ( event.getValueIsAdjusting() || ignoreSelectionChange.get() )
-	// return;
-
-	// ignoreSelectionChange.set( true );
-
-	// final int[] selectedRows = spotTable.getTable().getSelectedRows();
-	// final List< Spot > toSelect = new ArrayList<>( selectedRows.length );
-	// for ( final int row : selectedRows )
-	// toSelect.add( spotTable.getObjectForViewRow( row ) );
-
-	// selectionModel.clearSelection();
-	// selectionModel.addSpotToSelection( toSelect );
-	// refresh();
-
-	// ignoreSelectionChange.set( false );
-	// }
-	// }
 }
