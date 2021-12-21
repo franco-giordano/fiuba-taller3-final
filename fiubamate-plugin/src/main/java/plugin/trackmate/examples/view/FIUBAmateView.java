6bc6d3ff5e213b1d0bcfa349ed2171ddf8131bd4
Franco Giordano <fgiordano@fi.uba.ar>
2021-12-20 23:10:24 -0300

    fix forzar numeros con punto, sin importar el locale

--- fiubamate-plugin/src/main/java/plugin/trackmate/examples/view/FIUBAmateView.java
+++ fiubamate-plugin/src/main/java/plugin/trackmate/examples/view/FIUBAmateView.java
@@ -35,6 +35,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -562,7 +563,7 @@ public class FIUBAmateView extends JFrame
 		return new String[] {
 				String.valueOf(roi_index),
 				String.valueOf(tracksInRoi.size()),
-				String.format("%.4f", proportion),
+				String.format(Locale.ENGLISH, "%.4f", proportion),
 		};
 	}
 
